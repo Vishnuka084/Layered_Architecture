87bf672b515474e0f2bd9a8547d34a38307c6fea
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-02-10 22:24:31 +0530

    Reduced the complexcity of the QueryDAO

--- Layered_Architecture/src/dao/custom/impl/QueryDAOImpl.java
+++ Layered_Architecture/src/dao/custom/impl/QueryDAOImpl.java
@@ -14,30 +14,10 @@ public class QueryDAOImpl implements QueryDAO  {
 
     @Override
     public  ArrayList<CustomDTO> searchOrderByOrderID(String id)throws SQLException,ClassNotFoundException{
-        String sql="select Orders.oid,Orders.date,Orders.customerID,OrderDetails.itemCode,OrderDetails.qty,OrderDetails.unitPrice from Orders inner join OrderDetails on Orders.oid=OrderDetails.oid where Orders.oid=?;";
-        ResultSet rst = SQLUtil.executeQuery(sql,id);
+        ResultSet rst = SQLUtil.executeQuery("select Orders.oid,Orders.date,Orders.customerID,OrderDetails.itemCode,OrderDetails.qty,OrderDetails.unitPrice from Orders inner join OrderDetails on Orders.oid=OrderDetails.oid where Orders.oid=?;",id);
         ArrayList<CustomDTO> orderRecords =new ArrayList();
         while (rst.next()) {
-            String oid = rst.getString(1);
-            String date = rst.getString(2);
-            String customerID = rst.getString(3);
-            String itemCode = rst.getString(4);
-            int qty = rst.getInt(5);
-            BigDecimal unitPrice = rst.getBigDecimal(6);
-
-            CustomDTO customDTO =new CustomDTO();
-            customDTO.setOid(oid);
-            customDTO.setOrderDate(LocalDate.now());
-            customDTO.setCustomerId(customerID);
-            customDTO.setItemCode(itemCode);
-            customDTO.setQty(qty);
-            customDTO.setUnitPrice(unitPrice);
-
-            orderRecords.add(customDTO);
-
-
-
-
+            orderRecords.add(new CustomDTO(rst.getString(1),LocalDate.parse(rst.getString(2)),rst.getString(3),rst.getString(4),rst.getInt(5),rst.getBigDecimal(6)));
         }
         return orderRecords;
 
